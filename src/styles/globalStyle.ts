ca8bcac41e5edaef89c81bf147641652448569c3
kimsanglok <evertree6031@gmail.com>
2023-03-18 11:54:23 +0900

    style: amend globalStyle

--- src/styles/globalStyle.ts
+++ src/styles/globalStyle.ts
@@ -3,8 +3,33 @@ import normalize from 'emotion-normalize';
 import { css } from '@emotion/react';
 
 const globalStyle = css`
-  /* you can customize at this point. */
   ${normalize}
+
+  * {
+    box-sizing: border-box;
+  }
+
+  html {
+    font-size: 62.5%;
+  }
+
+  ol,
+  ul,
+  li {
+    margin: 0;
+    padding: 0;
+    list-style: none;
+  }
+
+  a {
+    text-decoration: none;
+
+    &:hover,
+    &:visited,
+    &:link {
+      color: black;
+    }
+  }
 `;
 
 export default globalStyle;
