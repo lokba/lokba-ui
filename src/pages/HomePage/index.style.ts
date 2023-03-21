071c616722a6d1829ee1814e1319fcba5242ea89
kimsanglok <evertree6031@gmail.com>
2023-03-22 05:55:23 +0900

    feat(HomePage): composite components and styling

--- src/pages/HomePage/index.style.ts
+++ src/pages/HomePage/index.style.ts
@@ -9,7 +9,6 @@ export const Container = styled.div`
 
     min-width: 20%;
     height: calc(100vh - 7rem);
-
     overflow-y: auto;
     overscroll-behavior: contain;
   }
@@ -17,5 +16,35 @@ export const Container = styled.div`
   > main {
     width: 80%;
     min-height: 100%;
+    display: flex;
+    padding: 1rem 5rem 10rem;
   }
+`;
+
+export const Left = styled.div`
+  width: 75%;
+`;
+
+export const Right = styled.div`
+  width: 25%;
+  padding: 5rem;
+
+  > div {
+    position: fixed;
+  }
+`;
+
+export const Title = styled.h2`
+  margin: 2rem 0;
+  font-size: 3.5rem;
+  border-bottom: 2px solid #e7ebf0;
+`;
+
+export const Description = styled.p`
+  font-size: 2rem;
+`;
+
+export const Sub = styled.h3`
+  margin: 1.5rem 0;
+  font-size: 3rem;
 `;
--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -1,24 +1,55 @@
 import { useParams } from 'react-router-dom';
 
-import { Sidebar } from '@/components';
+import { CodeBlock, Pagination, Sidebar, Spacing, TableOfContents } from '@/components';
 
 import { MENUS } from '@/constants';
 
 import * as S from './index.style';
 
 const HomePage = () => {
-  const { menu: currentMenu } = useParams();
+  const { menu: title } = useParams();
 
   return (
     <S.Container>
       <Sidebar menus={MENUS} />
       <main>
-        {[...Array(100)].map(() => (
-          <div>{currentMenu}</div>
-        ))}
+        <S.Left>
+          <S.Title>{title}</S.Title>
+          <S.Description>
+            {title} is the most abstract component on top of which all other Chakra UI components
+            are built. By default, it renders a `div` element
+          </S.Description>
+          <Spacing h={4} />
+          <S.Sub>Import</S.Sub>
+          <CodeBlock lang="jsx" code={`import { ${title} } from '@lokba-ui/react'`} />
+          <Spacing h={4} />
+          <S.Sub>Example</S.Sub>
+          <CodeBlock lang="jsx" code={EXAMPLE_CODE} />
+          <Spacing h={8} />
+          <Pagination
+            previous={{ label: 'Box', url: '/components/Box' }}
+            next={{ label: 'Container', url: '/components/Container' }}
+          />
+        </S.Left>
+        <S.Right>
+          <div>
+            <TableOfContents contents={[`${title}`, 'Import', 'Example']} />
+          </div>
+        </S.Right>
       </main>
     </S.Container>
   );
 };
+
+const EXAMPLE_CODE = `<ClickArea
+// CSS className
+className={className}
+// The function to invoke when clicked ('() => void')
+onClick={onClick}
+// If it can be clicked ('boolean')
+enabled={enabled}
+>
+{children}
+</ClickArea>`;
 
 export default HomePage;
