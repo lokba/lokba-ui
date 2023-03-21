fd39f8ff8a0a4fada73601201f80ae66fa48d762
kimsanglok <evertree6031@gmail.com>
2023-03-22 05:53:24 +0900

    feat: add TableOfContents component

---
+++ src/components/TableOfContents/index.style.ts
@@ -0,0 +1,14 @@
+import styled from '@emotion/styled';
+
+export const Container = styled.ul`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  padding: 1rem 1.6rem;
+  border-left: 2px solid #e7ebf0;
+
+  > li {
+    font-size: 2rem;
+    color: #868e96;
+  }
+`;
