9350b1a39153e1e75e426c31f2af7cc187758738
kimsanglok <evertree6031@gmail.com>
2023-03-23 19:41:19 +0900

    feat(@lokba-ui/react): add Text component

---
+++ packages/react/src/components/Text/index.tsx
@@ -0,0 +1,43 @@
+type LengthUnit = 'px' | 'em' | 'rem' | '%';
+type RGB = `rgb(${number}, ${number}, ${number})`;
+type RGBA = `rgba(${number}, ${number}, ${number}, ${number})`;
+type HEX = `#${string}`;
+
+type Props = {
+  as: keyof JSX.IntrinsicElements;
+  fontSize?: `${number}${LengthUnit}`;
+  color?: RGB | RGBA | HEX;
+  mt?: `${number}${LengthUnit}`;
+  mr?: `${number}${LengthUnit}`;
+  mb?: `${number}${LengthUnit}`;
+  ml?: `${number}${LengthUnit}`;
+  children?: React.ReactNode;
+};
+
+const Text = (props: Props) => {
+  const {
+    as = 'div',
+    fontSize,
+    color = '#000000',
+    mt = '0px',
+    ml = '0px',
+    mb = '0px',
+    mr = '0px',
+    children,
+  } = props;
+  const CustomTag = as;
+
+  return (
+    <CustomTag
+      style={{
+        fontSize: `${fontSize}`,
+        color: `${color}`,
+        margin: `${mt} ${mr} ${mb} ${ml}`,
+      }}
+    >
+      {children}
+    </CustomTag>
+  );
+};
+
+export default Text;
--- packages/react/src/components/index.ts
+++ packages/react/src/components/index.ts
@@ -1,2 +1,4 @@
 export { default as Spacing } from './Spacing';
 export { default as Divider } from './Divider';
+export { default as Heading } from './Heading';
+export { default as Text } from './Text';
